3d1da787cd51bed608bea6a052071ee1d0049ea0
EznXadee <eznzaidi@gmail.com>
2024-09-11 02:10:35 +0500

    redesigning

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,23 +3,16 @@ import './herosection.css';
 
 const HeroSection = () => {
   return (
-    <section className="hero">
-      <div className="hero-content">
-        <div className="hero-boxes">
-          <div className="box-1"> {/* First Box */}
-            <h2 className="box-title">Make it easy</h2>
-            <p className="box-text">Experience award-winning hosting 
-                solutions with seamless connectivity, streamlined 
-                performance, and premium support, delivering top-tier
-                 reliability and speed.</p>
-          </div>
-          <div className="box-2"> {/* Second Box */}
-            <h2 className="box-title">Box 2</h2>
-            <p className="box-text">This is the second box.</p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <section id="hero">
+                <div id="hero-margin">
+                <h1>Ideas That Drive 
+                </h1>
+                <h1>Change</h1>
+                <p>
+                At Sivda, our team of experts transforms the intricacies of web hosting into seamless digital solutions, empowering your online presence to thrive in the modern digital landscape.
+                </p>
+                </div>
+            </section>
   );
 };
 
